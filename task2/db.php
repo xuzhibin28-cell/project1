f9cc04d064b868b3f89c5d49ef397194797a25ca
KHOR ZHI BIN <xuzhibin28@gmail.com>
2026-04-21 13:39:51 +0800

    Fix database connection error message

--- task2/db.php
+++ task2/db.php
@@ -2,11 +2,11 @@
 $servername = "localhost"; 
 $username = "root";        
 $password = "";            
-$dbname = "quetion1";      // 这就是你照片里那个数据库的名字
+$dbname = "quetion1";      
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("数据库连接失败: " . $conn->connect_error);
+    die( db base error" . $conn->connect_error);
 } 
 ?>
